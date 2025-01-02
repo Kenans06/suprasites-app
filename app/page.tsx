bc2b240dc0ce8745cead181f6d13414877501cd4
Kenan Simsek <Kenan.Simsek@10forit.com>
2025-01-02 16:45:10 +0100

    add logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
+import Logo from "./logowtext.png";
+
 export default function Home() {
   return (
     <main>
+      <Image
+        className="block m-auto w-96"
+        src={Logo}
+        alt="Hairsalon SixtyOne Logo"
+        quality={100}
+        placeholder="blur"
+      ></Image>
       <div className="p-5 justify-center items-center bg-neutral-800 text-center text-white">
-        <h1>Supra Sites</h1>
         <p>
           Hier komt binnenkort je favoriete webontwikkelaar online! We zijn hard
           bezig met het ontwikkelen van onze website, dus heel even geduld!
