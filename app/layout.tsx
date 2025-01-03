6743b5737581d22212f714d287303ecc8359bf25
Kenan Simsek <Kenan.Simsek@10forit.com>
2025-01-03 21:56:36 +0100

    add new font

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./globals.css";
 
-const newRodin = localFont({ src: "./fonts/new-rodin.otf" });
+const newRodin = localFont({ src: "./fonts/newrodin-regular.otf" });
 
 export const metadata: Metadata = {
   title: "Supra Sites",
