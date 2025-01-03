953934f8db0bf519f179ccf0fc0bb33f0a1b30a5
Kenan Simsek <Kenan.Simsek@10forit.com>
2025-01-03 13:44:28 +0100

    add header and footer components

---
+++ app/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="min-h-10 p-10 bg-black">
+      <div className="text-center text-yellow-400 text-2xl">
+        Under Construction
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
