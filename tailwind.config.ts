adf910eed64ed929c53ce962fe8010d037d5d1db
Kenan Simsek <Kenan.Simsek@10forit.com>
2025-01-03 13:43:30 +0100

    add custom orange colour

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,7 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
         "supra-white": "#FFFDED",
+        "supra-orange": "#ff9400",
       },
     },
   },
