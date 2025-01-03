2c5563881ac48fcf71e19e0acb3179ffa0701c70
Kenan Simsek <Kenan.Simsek@10forit.com>
2025-01-03 12:48:30 +0100

    center div and add custom white

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,19 @@ import Logo from "./logowtext.png";
 
 export default function Home() {
   return (
-    <div className="content-center">
-      <Image
-        className="block m-auto w-96"
-        src={Logo}
-        alt="Hairsalon SixtyOne Logo"
-        quality={100}
-        placeholder="blur"
-      ></Image>
-      <div className="p-5 justify-center items-center text-center  text-gray-100">
-        <p>
+    <div className="flex h-screen">
+      <div className="m-auto">
+        <Image
+          className="block m-auto w-1/2"
+          src={Logo}
+          alt="Hairsalon SixtyOne Logo"
+          quality={100}
+          placeholder="blur"
+        ></Image>
+        <div className="block p-5 text-center text-3xl text-supra-white">
           Hier komt binnenkort je favoriete webontwikkelaar online! We zijn hard
           bezig met het ontwikkelen van onze website, dus heel even geduld!
-        </p>
+        </div>
       </div>
     </div>
   );
